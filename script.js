f2553e3526e8c5bc0e3b24210fd80707c47b1c16
iM Mustafa <im262897tt@gmail.com>
2025-02-20 20:19:51 -0800

    start javascript

---
+++ script.js
@@ -0,0 +1,12 @@
+// Quiz questions data
+const quizData = [
+    {
+      question: "What does HTML stand for?",
+      choices: [
+        "Hyper Text Markup Language",
+        "Home Tool Markup Language",
+        "Hyperlinks and Text Markup Language",
+        "Hyper Terminal Markup Language"
+      ],
+      answer: "Hyper Text Markup Language"
+    },
